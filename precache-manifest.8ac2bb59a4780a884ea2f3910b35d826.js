78ef8d6070ad0bd2d9036648d19c39ee40452146
don-arun <don-arun@users.noreply.github.com>
2020-04-23 19:52:14 +0530

    Updates

---
+++ precache-manifest.8ac2bb59a4780a884ea2f3910b35d826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53051c9b3ed56f2395facc5fd2068b3d",
+    "url": "/simplefolio/index.html"
+  },
+  {
+    "revision": "95d94bf5782045699215",
+    "url": "/simplefolio/static/css/main.9f76ad70.chunk.css"
+  },
+  {
+    "revision": "9876115fe9f12b066430",
+    "url": "/simplefolio/static/js/2.7bd5a37b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplefolio/static/js/2.7bd5a37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95d94bf5782045699215",
+    "url": "/simplefolio/static/js/main.67aa7a43.chunk.js"
+  },
+  {
+    "revision": "0937320d76969aeab6b3",
+    "url": "/simplefolio/static/js/runtime-main.3f634e07.js"
+  }
+]);
